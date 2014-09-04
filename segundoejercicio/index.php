e22f2415f2c5d0246bd9f856058de52ec29f3663
Santiago <taguianbassment@gmail.com>
2014-09-04 15:48:34 -0500

    Clase Web Sengundo Ejercicio
    Segundo ejercicio, HTML

---
+++ segundoejercicio/index.php
@@ -0,0 +1,37 @@
+<!Doctype html>
+<html lang="es">
+	<head>
+		<meta charset=""/>
+		<link rel="stylesheet" type="text/css" href=""/>
+		<script type="text/javascript"></script>
+	</head>
+	<body>
+		<header>
+			<figure>
+				<img src="https://scontent-a-mia.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/10647137_10152540995054823_6072398225126802162_n.jpg?oh=2e0ca9d217f37a74b0768f3a3e9e522c&oe=5474AFAC" alt="Mel Suicide" />
+				<h1>Internet is cats</h1>
+			</figure>
+		</header>
+		<nav>
+			<a href="http://vk.com/album-34908971_152348142">not Cats...#</a>
+			<ul>
+				<li><a href="https://fbcdn-sphotos-e-a.akamaihd.net/hphotos-ak-xpa1/v/t1.0-9/10441062_759591877415218_5462278722753248433_n.jpg?oh=90577f60f1c14a22fdaeaf16270e6142&oe=5476F2FE&__gda__=1416955718_e982e7271937b4f44b0e28c34d170df5">not more cats</a></li>
+				<li>cats cats cats</li>
+				<li>please, no more cats</li>
+			</ul>
+		</nav>
+		<section>
+			
+		</section>
+		<aside>
+			<ol>
+				<li><a target="_blank" href="http://suicidegirlsgallery.blogspot.com/2014/09/linzz.html?zx=4c35ba0bca26f3b2">Cat 1</a></li>
+				<li><a target="_blank" href="http://suicidegirlsgallery.blogspot.com/2014/08/lawita.html?zx=86e182f03f523eaf">Cat 2</a></li>
+				<li><a target="_blank" href="http://suicidegirlsgallery.blogspot.com/2014/08/babel.html?zx=db7de8c6f75b9069">Cat 3</a></li>
+			</ol>
+		</aside>
+		<footer>
+			
+		</footer>
+	</body>
+</html>	
