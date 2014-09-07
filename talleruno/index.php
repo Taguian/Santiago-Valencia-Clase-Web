c226b59e35ae5cc1fe510a5b63986fd3e146732a
Santiago <taguianbassment@gmail.com>
2014-09-07 17:35:30 -0500

    Taller Uno - Inicio
    inicio del taller numero 1
    
    -Sistema de registro

---
+++ talleruno/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<form action="includes/registrar.php" method="post">
+			<table width="200" border="0">
+				<tr>
+					<td>Nombre:</td>
+					<td><input type="text" name="nombre" /></td>
+				</tr>
+				<tr>
+					<td>Usuario:</td>
+					<td><input type="text" name="usuario" /></td>
+				</tr>
+				<tr>
+					<td>Contraseña</td>
+					<td><input type="text" name="password" /></td>
+				</tr>
+				<tr>
+					<td>Confirmar contraseña</td>
+					<td><input type="text" name="passworddos" /></td>
+				</tr>
+				<tr>
+					<td>E-mail</td>
+					<td><input type="text" name="email" /></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td><input type="submit" value="Registrar" /></td>
+				</tr>
+			</table>	
+		</form>
+	</body>
+</html>
