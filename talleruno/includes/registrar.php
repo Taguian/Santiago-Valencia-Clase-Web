9f0862914a809f70f928de5125b01e8f45044dbe
Santiago <taguianbassment@gmail.com>
2014-09-11 10:57:11 -0500

    Taller Uno Terminado
    Termine

--- talleruno/includes/registrar.php
+++ talleruno/includes/registrar.php
@@ -1,4 +1,29 @@
 <?php
+ 	$nombre =$_POST["nombre"];
+ 	$user =$_POST["usuario"];
+    $password =$_POST["password"];
+    $passworddos=$_POST["passworddos"];
+    $email =$_POST["email"];
+    include_once("connect.php");
+
+    if($password==$passworddos){//Si ambas contrasenas insertadas coinciden...paso a realizar el registro
+
+        $registrar="INSERT INTO talleruno.usuarios(`nombre`, `usuario`, `password`, `email`) VALUES ('$nombre','$user','$password','$email')";
+        $comunicacion= mysqli_query($con,$registrar);
+        echo "Ususario registrado satisfactoriamente";
+    }else{
+        echo"<h2>Error en el registro, revise que ambas contraseñas coincidan</h2>";
+    }
+    if($comunicacion == false)
+    {
+    	echo "Error en la comunicacion";
+      	echo "Error: ".mysqli_error($con)."";
+  } 
+
+
+/*
+trare de seguir un tutorial en linea a ver si era mejor, pero finalmente no sirvio.
+mejor voy a hacer el registro como lo hice en el primer trabajo.La idea era que no me dejara registrar si los campos estaban vacios
 	include("connect.php");
 	if(isset($_POST['nombre']) && !empty($_POST['nombre']) &&
 	isset($_POST['usuario']) && !empty($_POST['usuario']) &&
@@ -19,4 +44,5 @@
 		echo "Verificar que cada campo haya sido llenado correctamente";
 
 	}
+	*/
 ?>
--- talleruno/includes/verificar.php
+++ talleruno/includes/verificar.php
@@ -1,4 +1,27 @@
 <?php 
+	$user =$_POST["usuario"];
+    $password =$_POST["password"];
+    include_once("connect.php");
+
+    $validar="SELECT * FROM talleruno.usuarios WHERE talleruno.usuarios.usuario='$user' AND talleruno.usuarios.password='$password'";
+    $enviar = mysqli_query($con,$validar);
+
+
+    /*En esta parte me ayudo un compañero de clase, igual que en el primer ejercicio no recordaba como pasar de una pagina a otra*/
+    if(mysqli_num_rows($enviar) < 1)
+    {
+    	echo "Infromacion incorrecta";
+    	echo"<meta http-equiv='refresh' content='6;url=/talleruno/index.php'>";//no funciona, ni idea por que.
+    }
+
+    else{
+        echo"<meta http-equiv='refresh' content='0.5;url=/claseweb/Santiago-Valencia-Clase-Web/talleruno/perfil.php?usuario=".$user."'>";
+    }
+
+
+
+/*Igual aqui, queria usar un titurial de como hacer una sesion, pero tampoco funciono, depronto seria bueno mirarlo en clase para ver que estaba pasando
+
 	session_start();
 	include("connect.php");
 	if (isset($_POST['usuario']) && !empty($_POST['usuario']) &&
@@ -18,4 +41,5 @@
 	}else{
 		echo "Llenar ambos campos para poder hacer log in";
 	}
+	*/
  ?>
