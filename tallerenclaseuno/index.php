87bfaa692235b5b57c607583c12f08da2f8e36fe
Santiago <taguianbassment@gmail.com>
2014-09-18 16:49:47 -0500

    Fail
    Fail

---
+++ tallerenclaseuno/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Trabajo en clase CSS HTML</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Aguafina+Script|PT+Sans' rel='stylesheet' type='text/css'>
+	<meta name="viewport" content="width=device-width">
+</head>
+<body>
+	<header>
+		<div id="headerTitle">
+			<figure>
+				<img src="includes/images/Profile.jpg">
+			</figure>
+			<h2>...bla bla bla bla echilada, bla bla bla...</h2>
+		</div>
+	</header>
+	<section>
+		<article>
+			<figure>
+				<img src="">
+			</figure>
+				<h2></h2>
+			<p>The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee. </p>
+			<footer>
+				
+			</footer>
+		</article>
+	</section>
+	<footer>
+		<p>By <a href="http://twitter.com/Taguian">@Taguian</a></p>
+	</footer>
+</body>
+</html>
