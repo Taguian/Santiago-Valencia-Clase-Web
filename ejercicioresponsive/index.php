bda30776f5eafbb78beea0718205141d913d1190
Santiago <taguianbassment@gmail.com>
2014-10-16 16:56:49 -0500

    Trabajo Maestro sobre gatos
    Template con bootsrap y todo es chevere

---
+++ ejercicioresponsive/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Trabajo en clase CSS HTML</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Aguafina+Script|PT+Sans' rel='stylesheet' type='text/css'>
+	<meta name="viewport" content="width=device-width">
+</head>
+<body>
+	<header>
+		<figure>
+			<img src="includes/logo.jpg">
+		</figure>
+		<div id="headerTitle">
+			<h1>Ejercicio en Clase</h1>
+			<h2>Front end motherfucker</h2>
+			<h3>Practica de integracion html con css</h3>
+		</div>
+	</header>
+	<nav>
+		<ul>
+			<li><a href="#">HOME</a></li>
+			<li><a href="#">ABOUT</a></li>
+			<li><a href="#">CONTACT</a></li>
+		</ul>
+	</nav>
+	<section>
+		<article>
+			<figure class="avatar">
+				<img src="includes/images/girlone.jpg">
+			</figure>
+				<h2>Motherfucking Post</h2>
+			<p>The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee. </p>
+			<footer>
+			</footer>
+		</article>
+		<article>
+			<figure>
+				<img src="includes/images/girltwo.jpg">
+			</figure>
+			<h2>Motherfucking Post Number Two</h2>
+			<p>The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee. </p>
+			<footer>
+			</footer>
+		</article>
+		<article>
+			<figure>
+				<img src="includes/images/girlthree.jpg">
+			</figure>
+			<h2>Motherfucking Post Number Three</h2>
+			<p>The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee. </p>
+			<footer>
+			</footer>
+		</article>
+	</section>
+	<footer>
+		<p>By <a href="http://twitter.com/Taguian">@Taguian</a></p>
+	</footer>
+</body>
+</html>
